eef42247a254bc232d0276794bbab083e3c8f9b8
Sven <s.fehler@pbsnetwork.eu>
2019-11-11 16:59:51 +0100

    more stable IP resolver

--- src/resolveIP.js
+++ src/resolveIP.js
@@ -11,7 +11,7 @@ jsl.unused(http);
  * @returns {String}
  */
 const resolveIP = req => {
-    let ipaddr = "err";
+    let ipaddr = null;
 
     try
     {
@@ -24,10 +24,38 @@ const resolveIP = req => {
     }
     catch(err)
     {
-        ipaddr = "err";
+        ipaddr = null;
     }
 
-    return (ipaddr.length<15?ipaddr:(ipaddr.substr(0,7)==='::ffff:'?ipaddr.substr(7):"err"));
+    if(jsl.isEmpty(ipaddr))
+    {
+        if(!jsl.isEmpty(req.headers["cf_connecting_ip"]) && (isValidIP(req.headers["cf_connecting_ip"]) || isValidIP(req.headers["cf_connecting_ip"])))
+            ipaddr = req.headers["cf_connecting_ip"];
+        else if(!jsl.isEmpty(req.headers["x_real_ip"]) && (isValidIP(req.headers["x_real_ip"]) || isValidIP(req.headers["x_real_ip"])))
+            ipaddr = req.headers["x_real_ip"];
+        else ipaddr = "err";
+    }
+
+    ipaddr = (ipaddr.length<15?ipaddr:(ipaddr.substr(0,7)==='::ffff:'?ipaddr.substr(7):"err"));
+
+    try
+    {
+        return typeof ipaddr == "string" ? ipaddr : ipaddr.toString();
+    }
+    catch(err)
+    {
+        return "err";
+    }
     //return "blacklist_test";
 };
+
+const isValidIP = ip => {
+    // thanks to https://nbviewer.jupyter.org/github/rasbt/python_reference/blob/master/tutorials/useful_regex.ipynb
+    let ipv4regex = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/gm;
+    let ipv6regex = /^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$/gm;
+
+    return (ip.match(ipv4regex) || ip.match(ipv6regex));
+}
+
 module.exports = resolveIP;
+module.exports.isValidIP = isValidIP;
