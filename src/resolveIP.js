bc05e165981c724feaea0b8b5a4cc730b4d8a5b4
Sven <s.fehler@pbsnetwork.eu>
2019-12-13 17:43:09 +0100

    more stability in IP resolver

--- src/resolveIP.js
+++ src/resolveIP.js
@@ -17,10 +17,16 @@ const resolveIP = req => {
 
     try
     {
-        if(!jsl.isEmpty(req.headers) && !jsl.isEmpty(req.headers["x-forwarded-for"]) && settings.httpServer.reverseProxy)
+        if(!jsl.isEmpty(req.headers) && !jsl.isEmpty(req.headers["x-forwarded-for"]) && settings.httpServer.reverseProxy) // format: <client>, <proxy1>, <proxy2>
         {
             ipaddr = req.headers["x-forwarded-for"]; // I have to use the X-Forwarded-For header because I'm using a reverse proxy
-            if(ipaddr.includes(",")) ipaddr = ipaddr.split(",")[0];
+            let ipSplit = ipaddr.split(/[,]\s*/gm);
+            if(ipaddr.includes(","))
+                ipaddr = ipSplit[0]; // try to get IP from <client>
+            if(!isValidIP(ipaddr))
+                ipaddr = ipSplit[1]; // if <client> IP is invalid, try <proxy1> instead
+            if(!isValidIP(ipaddr))
+                ipaddr = req.connection.remoteAddress; // else just default to the remote IP
         }
         else ipaddr = req.connection.remoteAddress;
     }
@@ -29,21 +35,27 @@ const resolveIP = req => {
         ipaddr = null;
     }
 
-    if(jsl.isEmpty(ipaddr))
+    ipaddr = ipaddr.trim();
+
+    if(jsl.isEmpty(ipaddr)) // if the reverse proxy didn't work, try getting the IP from the Cloudflare headers
     {
-        if(!jsl.isEmpty(req.headers["cf_connecting_ip"]) && (isValidIP(req.headers["cf_connecting_ip"]) || isValidIP(req.headers["cf_connecting_ip"])))
+        if(!jsl.isEmpty(req.headers["cf_connecting_ip"]) && (isValidIP(req.headers["cf_connecting_ip"]) || isValidIP(req.headers["cf_connecting_ip"]))) // Cloudflare
             ipaddr = req.headers["cf_connecting_ip"];
-        else if(!jsl.isEmpty(req.headers["x_real_ip"]) && (isValidIP(req.headers["x_real_ip"]) || isValidIP(req.headers["x_real_ip"])))
+        else if(!jsl.isEmpty(req.headers["x_real_ip"]) && (isValidIP(req.headers["x_real_ip"]) || isValidIP(req.headers["x_real_ip"]))) // Cloudflare
             ipaddr = req.headers["x_real_ip"];
+        else if(!jsl.isEmpty(req.headers["x-proxyuser-ip"]) && (isValidIP(req.headers["x-proxyuser-ip"]) || isValidIP(req.headers["x-proxyuser-ip"]))) // Google services
+            ipaddr = req.headers["x-proxyuser-ip"];
         else ipaddr = "err";
     }
 
-    ipaddr = (ipaddr.length<15?ipaddr:(ipaddr.substr(0,7)==='::ffff:'?ipaddr.substr(7):"err"));
+    ipaddr = (ipaddr.length < 15 ? ipaddr : (ipaddr.substr(0,7) === "::ffff:" ? ipaddr.substr(7) : "err"));
 
     try
     {
-        if(settings.httpServer.ipHashing.enabled)
+        if(settings.httpServer.ipHashing.enabled && isValidIP(ipaddr))
             ipaddr = hashIP(ipaddr);
+        else if(settings.httpServer.ipHashing.enabled)
+            ipaddr = "err";
         return typeof ipaddr == "string" ? ipaddr : ipaddr.toString();
     }
     catch(err)
@@ -52,7 +64,10 @@ const resolveIP = req => {
     }
 };
 
-const isValidIP = ip => (ip.match(settings.httpServer.regexes.ipv4) || ip.match(settings.httpServer.regexes.ipv6));
+const ipv4regex = settings.httpServer.regexes.ipv4;
+const ipv6regex = settings.httpServer.regexes.ipv6;
+
+const isValidIP = ip => (ip.match(ipv4regex) || ip.match(ipv6regex));
 const hashIP = ip => crypto.createHash(settings.httpServer.ipHashing.algorithm).update(ip, "utf8").digest(settings.httpServer.ipHashing.digest).toString();
 
 module.exports = resolveIP;
