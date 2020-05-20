8a0ab08e27fac6aead157144296e5c30a901a2db
Sv443 <sven.fehler@web.de>
2020-05-20 23:16:56 +0200

    Fixed #92

--- tools/validate-jokes.js
+++ tools/validate-jokes.js
@@ -5,12 +5,12 @@ const settings = require("../settings");
 // TODO: rework this
 
 
-console.log(`\nValidating jokes in "${settings.jokes.jokesFilePath}"...\n`);
+console.log(`\nValidating jokes-xy.json files in "${settings.jokes.jokesFolderPath}"...\n`);
 
 require("../src/parseJokes").init().then(() => {
-    console.log(`\n${jsl.colors.fg.green}All jokes are valid.${jsl.colors.rst}\n`);
+    console.log(`${jsl.colors.fg.green}All jokes are valid.${jsl.colors.rst}\n`);
     process.exit(0);
 }).catch(err => {
-    console.log(`\n${jsl.colors.fg.red}${err}${jsl.colors.rst}\n`);
+    console.log(`${jsl.colors.fg.red}${err}${jsl.colors.rst}\n`);
     process.exit(1);
 });
