7b1643683cafdb12f53f1ee55967b9948a5bfe0b
Sven <s.fehler@pbsnetwork.eu>
2020-07-08 15:46:25 +0200

    fixed validate-ids script

--- tools/validate-ids.js
+++ tools/validate-ids.js
@@ -1,9 +1,7 @@
 // this validates all jokes' IDs. This will be run through the CI to make sure the IDs are correct
 // run this with the command "npm run reassign-ids"
 
-
-// TODO: rework this
-
+const { resolve, join } = require("path");
 
 const exitWithError = (msg, err) => {
     console.log(`\n\n\x1b[31m\x1b[1m>> ${msg}:\n${err}\n\n\x1b[0m`);
@@ -15,40 +13,60 @@ try
     const fs = require("fs-extra");
     const settings = require("../settings");
 
-    console.log(`\nValidating joke IDs in file "${settings.jokes.jokesFilePath}"...`);
+    console.log(`\nValidating joke IDs in files in "${settings.jokes.jokesFolderPath}"...`);
 
-    let initialJokes = JSON.parse(fs.readFileSync(settings.jokes.jokesFilePath).toString()).jokes;
-    let initialInfo = JSON.parse(fs.readFileSync(settings.jokes.jokesFilePath).toString()).info;
+    let validatedFiles = 0;
+    let notOk = 0;
 
-    if(initialInfo.formatVersion != settings.jokes.jokesFormatVersion)
-        return exitWithError("Error while checking format version", `Format version in file "${settings.jokes.jokesFilePath}" (version ${initialInfo.formatVersion}) is different from the one being currently used in JokeAPI (${settings.jokes.jokesFormatVersion})`);
+    fs.readdirSync(settings.jokes.jokesFolderPath).forEach(fName => {
+        if(fName.startsWith("template"))
+            return;
 
-    let erroredJokes = [];
+        let langCode = fName.split("-")[1].substr(0, 2);
 
-    initialJokes.forEach((joke, i) => {
-        if(joke.id != i)
-            erroredJokes.push({joke: joke, idx: i});
+        let filePath = resolve(join(settings.jokes.jokesFolderPath, fName));
+
+        let jokeFileObj = JSON.parse(fs.readFileSync(filePath).toString());
+        let initialJokes = jokeFileObj.jokes;
+        let initialInfo = jokeFileObj.info;
+
+        if(initialInfo.formatVersion != settings.jokes.jokesFormatVersion)
+            return exitWithError("Error while checking format version", `Format version in file "${filePath}" (version ${initialInfo.formatVersion}) is different from the one being currently used in JokeAPI (${settings.jokes.jokesFormatVersion})`);
+
+        let erroredJokes = [];
+
+        initialJokes.forEach((joke, i) => {
+            if(joke.id != i)
+                erroredJokes.push({joke: joke, idx: i});
+        });
+
+        validatedFiles++;
+
+        if(erroredJokes.length != 0)
+        {
+            console.log(`\n\n\x1b[31m\x1b[1mInvalid joke ID${erroredJokes.length > 1 ? "s" : ""} found:\x1b[0m\n`);
+            erroredJokes.forEach(errjoke => {
+                let jokeContent = "";
+                if(errjoke.joke.type == "single")
+                    jokeContent = errjoke.joke.joke.replace(/\n/gm, "\\n");
+                else if(errjoke.joke.type == "twopart")
+                    jokeContent = `${errjoke.joke.setup.replace(/\n/gm, "\\n")} - ${errjoke.joke.delivery.replace(/\n/gm, "\\n")}`;
+
+                console.log(`#${errjoke.joke.id} | ${langCode} | ${errjoke.joke.category} | ${jokeContent}    (Expected ID #${errjoke.idx} - joke instead has #${errjoke.joke.id})`);
+                notOk++;
+            });
+        }
     });
 
-    if(erroredJokes.length == 0)
+    if(notOk > 0)
     {
-        console.log(`\x1b[32m\x1b[1mDone validating IDs of all ${initialJokes.length} jokes.\n\x1b[0m`);
-        process.exit(0);
+        console.log(`\n\x1b[33m\x1b[1mYou can run the command "npm run reassign-ids" to correct all joke IDs\n\x1b[0m`);
+        process.exit(1);
     }
     else
     {
-        console.log(`\n\n\x1b[31m\x1b[1mInvalid joke ID${erroredJokes.length > 1 ? "s" : ""} found:\x1b[0m\n`);
-        erroredJokes.forEach(errjoke => {
-            let jokeContent = "";
-            if(errjoke.joke.type == "single")
-                jokeContent = errjoke.joke.joke.replace(/\n/gm, "\\n");
-            else if(errjoke.joke.type == "twopart")
-                jokeContent = `${errjoke.joke.setup.replace(/\n/gm, "\\n")} - ${errjoke.joke.delivery.replace(/\n/gm, "\\n")}`;
-
-            console.log(`> #${errjoke.joke.id} | ${errjoke.joke.category} | ${jokeContent}    (Expected ID #${errjoke.idx} - joke instead has #${errjoke.joke.id})`);
-        });
-        console.log(`\n\x1b[33m\x1b[1mYou can run the command "npm run reassign-ids" to correct all joke IDs\n\x1b[0m`);
-        process.exit(1);
+        console.log(`\x1b[32m\x1b[1mDone validating IDs of all ${validatedFiles} files.\n\x1b[0m`);
+        process.exit(0);
     }
 }
 catch(err)
