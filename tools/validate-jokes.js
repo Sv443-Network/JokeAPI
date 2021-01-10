2c961f30cc50ff4c30992b9a3bd123f70ad08dd4
Sven <s.fehler@pbsnetwork.eu>
2021-01-10 17:39:03 +0100

    fixed validate-jokes script

--- tools/validate-jokes.js
+++ tools/validate-jokes.js
@@ -5,12 +5,14 @@ const settings = require("../settings");
 
 console.log(`\nValidating jokes-xy.json files in "${settings.jokes.jokesFolderPath}"...\n`);
 
-require("../src/translate").init().then(() => {
-    require("../src/parseJokes").init().then(() => {
-        console.log(`${jsl.colors.fg.green}All jokes are valid.${jsl.colors.rst}\n`);
-        process.exit(0);
-    }).catch(err => {
-        console.log(`${jsl.colors.fg.red}${err}${jsl.colors.rst}\n`);
-        process.exit(1);
+require("../src/languages").init().then(() => {
+    require("../src/translate").init().then(() => {
+        require("../src/parseJokes").init().then(() => {
+            console.log(`${jsl.colors.fg.green}All jokes are valid.${jsl.colors.rst}\n`);
+            process.exit(0);
+        }).catch(err => {
+            console.log(`${jsl.colors.fg.red}${err}${jsl.colors.rst}\n`);
+            process.exit(1);
+        });
     });
 });
