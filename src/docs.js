c69ff6148c5c821884618fb33b40ea08b3599a24
Sv443 <sven.fehler@web.de>
2021-01-12 17:02:41 +0100

    switched from JSL to SCL in docs.js

--- src/docs.js
+++ src/docs.js
@@ -1,8 +1,7 @@
 // this module initializes the blacklist, whitelist and console blacklist
 
-const jsl = require("svjsl");
 const scl = require("svcorelib");
-const farmhash = require("farmhash");
+// const farmhash = require("farmhash");
 const fs = require("fs-extra");
 const settings = require("../settings");
 const debug = require("./verboseLogging");
@@ -14,6 +13,7 @@ const xss = require("xss");
 const semver = require("semver");
 const analytics = require("./analytics");
 const languages = require("./languages");
+const path = require("path");
 
 
 /**
@@ -43,39 +43,58 @@ function init()
  */
 function startDaemon()
 {
-    let oldChecksum = "";
-    let newChecksum = "";
+    // See https://github.com/Sv443/SvCoreLib/issues/6 on why I set the blacklist pattern to [ "**/**/invalid" ]
+    let fd = new scl.FolderDaemon(path.resolve(settings.documentation.rawDirPath), [ "**/path/that_doesnt/exist/*" ], true, settings.documentation.daemonInterval * 1000);
+    fd.onChanged((error, result) => {
+        scl.unused(result);
+        if(!error)
+        {
+            debug("Daemon", "Noticed changed files");
+            logRequest("docsrecompiled");
+            recompileDocs();
+        }
+    });
+    // See also https://github.com/Sv443/SvCoreLib/issues/7 (why does software break smh)
 
-    const scanDir = () => {
-        fs.readdir(settings.documentation.rawDirPath, (err, files) => {
-            if(err)
-                return console.log(`${jsl.colors.fg.red}Daemon got error: ${err}${jsl.colors.rst}\n`);
 
-            let checksum = "";
-            files.forEach((file, i) => {
-                checksum += (i != 0 && i < files.length ? "-" : "") + farmhash.hash32(fs.readFileSync(`${settings.documentation.rawDirPath}${file}`)).toString();
-            });
 
-            newChecksum = checksum;
-            if(jsl.isEmpty(oldChecksum))
-                oldChecksum = checksum;
+
+
+    // old code in case of an emergency:
+
+    // let oldChecksum = "";
+    // let newChecksum = "";
+
+    // const scanDir = () => {
+    //     fs.readdir(settings.documentation.rawDirPath, (err, files) => {
+    //         if(err)
+    //             return console.log(`${scl.colors.fg.red}Daemon got error: ${err}${scl.colors.rst}\n`);
+
+    //         let checksum = "";
+    //         files.forEach((file, i) => {
+    //             checksum += (i != 0 && i < files.length ? "-" : "") + farmhash.hash32(fs.readFileSync(`${settings.documentation.rawDirPath}${file}`)).toString();
+    //         });
+
+    //         newChecksum = checksum;
+    //         if(scl.isEmpty(oldChecksum))
+    //             oldChecksum = checksum;
             
-            if(oldChecksum != newChecksum)
-            {
-                debug("Daemon", "Noticed changed files");
-                logRequest("docsrecompiled");
-                recompileDocs();
-            }
+    //         if(oldChecksum != newChecksum)
+    //         {
+    //             debug("Daemon", "Noticed changed files");
+    //             logRequest("docsrecompiled");
+    //             recompileDocs();
+    //         }
 
-            oldChecksum = checksum;
-        });
-    };
+    //         oldChecksum = checksum;
+    //     });
+    // };
 
-    if(jsl.isEmpty(process.jokeapi.documentation))
-        process.jokeapi.documentation = {};
-    process.jokeapi.documentation.daemonInterval = setInterval(() => scanDir(), settings.documentation.daemonInterval * 1000);
+    // if(scl.isEmpty(process.jokeapi.documentation))
+    //     process.jokeapi.documentation = {};
+    // process.jokeapi.documentation.daemonInterval = setInterval(() => scanDir(), settings.documentation.daemonInterval * 1000);
 
-    scanDir();
+    // scanDir();
 }
 
 /**
@@ -110,21 +129,21 @@ function recompileDocs()
 
         filesToInject.forEach((fti, i) => {
             promises.push(new Promise((resolve, reject) => {
-                jsl.unused(reject);
+                scl.unused(reject);
                 inject(fti).then((injected, injectionsNum) => {
-                    if(!jsl.isEmpty(injectionsNum) && !isNaN(parseInt(injectionsNum)))
+                    if(!scl.isEmpty(injectionsNum) && !isNaN(parseInt(injectionsNum)))
                         process.injectionCounter += parseInt(injectionsNum);
 
                     process.brCompErrOnce = false;
 
                     if(settings.httpServer.encodings.gzip)
-                        saveEncoded("gzip", injectedFileNames[i], injected).catch(err => jsl.unused(err));
+                        saveEncoded("gzip", injectedFileNames[i], injected).catch(err => scl.unused(err));
                     if(settings.httpServer.encodings.deflate)
-                        saveEncoded("deflate", injectedFileNames[i], injected).catch(err => jsl.unused(err));
+                        saveEncoded("deflate", injectedFileNames[i], injected).catch(err => scl.unused(err));
                     if(settings.httpServer.encodings.brotli)
                     {
                         saveEncoded("brotli", injectedFileNames[i], injected).catch(err => {
-                            jsl.unused(err);
+                            scl.unused(err);
 
                             if(!process.brCompErrOnce)
                             {
@@ -145,7 +164,7 @@ function recompileDocs()
         });
 
         Promise.all(promises).then(() => {
-            debug("Docs", `Done recompiling docs in ${jsl.colors.fg.yellow}${new Date().getTime() - process.injectionTimestamp}ms${jsl.colors.rst}, injected ${jsl.colors.fg.yellow}${process.injectionCounter}${jsl.colors.rst} values`);
+            debug("Docs", `Done recompiling docs in ${scl.colors.fg.yellow}${new Date().getTime() - process.injectionTimestamp}ms${scl.colors.rst}, injected ${scl.colors.fg.yellow}${process.injectionCounter}${scl.colors.rst} values`);
         }).catch(err => {
             console.log(`Injection error: ${err}`);
         });
@@ -224,7 +243,7 @@ function saveEncoded(encoding, filePath, content)
  */
 function injectError(err, exit = true)
 {
-    console.log(`\n${jsl.colors.fg.red}Error while injecting values into docs: ${err}${jsl.colors.rst}\n`);
+    console.log(`\n${scl.colors.fg.red}Error while injecting values into docs: ${err}${scl.colors.rst}\n`);
     analytics({
         type: "Error",
         data: {
@@ -264,17 +283,17 @@ function inject(filePath)
                     "%#INSERT:DESC#%":                  settings.info.desc.toString(),
                     "%#INSERT:AUTHORWEBSITEURL#%":      settings.info.author.website.toString(),
                     "%#INSERT:AUTHORGITHUBURL#%":       settings.info.author.github.toString(),
-                    "%#INSERT:CONTRIBUTORS#%":          (!jsl.isEmpty(contributors) ? contributors : "{}"),
+                    "%#INSERT:CONTRIBUTORS#%":          (!scl.isEmpty(contributors) ? contributors : "{}"),
                     "%#INSERT:CONTRIBUTORGUIDEURL#%":   settings.info.contribGuideUrl.toString(),
                     "%#INSERT:PROJGITHUBURL#%":         settings.info.projGitHub.toString(),
                     "%#INSERT:JOKESUBMISSIONURL#%":     settings.jokes.jokeSubmissionURL.toString(),
                     "%#INSERT:CATEGORYARRAY#%":         JSON.stringify([settings.jokes.possible.anyCategoryName, ...settings.jokes.possible.categories]),
                     "%#INSERT:FLAGSARRAY#%":            JSON.stringify(settings.jokes.possible.flags),
                     "%#INSERT:FILEFORMATARRAY#%":       JSON.stringify(settings.jokes.possible.formats.map(itm => itm.toUpperCase())),
-                    "%#INSERT:TOTALJOKES#%":            (!jsl.isEmpty(jokeCount) ? jokeCount.toString() : 0),
-                    "%#INSERT:TOTALJOKESZEROINDEXED#%": (!jsl.isEmpty(jokeCount) ? (jokeCount - 1).toString() : 0),
+                    "%#INSERT:TOTALJOKES#%":            (!scl.isEmpty(jokeCount) ? jokeCount.toString() : 0),
+                    "%#INSERT:TOTALJOKESZEROINDEXED#%": (!scl.isEmpty(jokeCount) ? (jokeCount - 1).toString() : 0),
                     "%#INSERT:PRIVACYPOLICYURL#%":      settings.info.privacyPolicyUrl.toString(),
-                    "%#INSERT:DOCSURL#%":               (!jsl.isEmpty(settings.info.docsURL) ? settings.info.docsURL : "(Error: Documentation URL not defined)"),
+                    "%#INSERT:DOCSURL#%":               (!scl.isEmpty(settings.info.docsURL) ? settings.info.docsURL : "(Error: Documentation URL not defined)"),
                     "%#INSERT:RATELIMITCOUNT#%":        settings.httpServer.rateLimiting.toString(),
                     "%#INSERT:FORMATVERSION#%":         settings.jokes.jokesFormatVersion.toString(),
                     "%#INSERT:MAXPAYLOADSIZE#%":        settings.httpServer.maxPayloadSize.toString(),
@@ -291,7 +310,7 @@ function inject(filePath)
                 const checkMatch = (key, regex) => {
                     allMatches += ((file.toString().match(regex) || []).length || 0);
                     let injection = sanitize(injections[key]);
-                    file = file.replace(regex, !jsl.isEmpty(injection) ? injection : "Error");
+                    file = file.replace(regex, !scl.isEmpty(injection) ? injection : "Error");
                 };
 
                 let allMatches = 0;
