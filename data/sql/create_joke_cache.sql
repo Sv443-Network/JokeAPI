743dd32830f5abdbdd5101df82aed4563bd411b5
Sv443 <sven.fehler@web.de>
2021-02-15 22:06:54 +0100

    added milliseconds to DateTime column

--- data/sql/create_joke_cache.sql
+++ data/sql/create_joke_cache.sql
@@ -2,7 +2,7 @@ CREATE TABLE joke_cache (
 	`ClientIpHash` VARCHAR(96) NOT NULL COLLATE 'utf8_bin',
 	`JokeID` INT(6) NOT NULL COLLATE 'utf8_bin',
 	`LangCode` VARCHAR(2) NOT NULL COLLATE 'utf8_bin',
-	`DateTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`DateTime` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`ClientIpHash`, `JokeID`, `LangCode`)
 )
 COLLATE='utf8_bin';
