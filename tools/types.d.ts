e2f91b0a8dc7eef01cc6dcd8e2c82243f8b39564
Sven <s.fehler@pbsnetwork.eu>
2021-11-03 13:29:21 +0100

    types

--- tools/types.d.ts
+++ tools/types.d.ts
@@ -1,12 +1,31 @@
+// This file contains all types needed by the CLI tools in this folder
+
 import { Joke, JokeSubmission } from "../src/types/jokes";
 import { LangCode } from "../src/types/languages";
 
 
+//#MARKER generic
+
+/** Contains all info about a pressed key from the `keypress` package */
+export interface Keypress {
+    /** Final character(s) that would've been printed with the current key combination (e.g. `shift + a` => `A`) */
+    name: string;
+    /** Whether the control / CTRL key was held down */
+    ctrl: boolean;
+    /** Whether the meta / ALT key was held down */
+    meta: boolean;
+    /** Whether the shift key was held down */
+    shift: boolean;
+    /** If the keys produce an escape sequence, it will be put in here */
+    sequence?: string;
+    /** Some other kind of code that only appears sometimes, idk */
+    code?: string;
+}
+
+
 //#MARKER submissions
 
-/**
- * A single joke submission
- */
+/** A single joke submission */
 export interface Submission {
     /** The submission itself */
     joke: JokeSubmission & { safe: boolean };
@@ -20,9 +39,7 @@ export interface Submission {
     path: string;
 }
 
-/**
- * This object contains all submissions
- */
+/** This object contains all submissions */
 export type AllSubmissions = {
     [key in LangCode]?: Submission;
 };
@@ -46,15 +63,6 @@ export interface ReadSubmissionsResult {
 
 export type LastEditedSubmission = "accepted_safe" | "accepted_unsafe" | "edited" | "deleted";
 
-export interface Keypress {
-    name: string;
-    ctrl: boolean;
-    meta: boolean;
-    shift: boolean;
-    sequence?: string;
-    code?: string;
-}
-
 export interface ClientColorMapping {
     [/**Color code*/ key: string]: /**Client IP hashes*/ string[];
 }
@@ -63,6 +71,7 @@ export interface ClientColorMapping {
 
 export type AddJoke = Joke & { formatVersion: number, lang: LangCode, safe: boolean };
 
+/** Makes all properties `P` in object `T` also accept `null` as a value */
 export type NullableObj<T> = {
     [P in keyof T]: (T[P] | null);
 };
