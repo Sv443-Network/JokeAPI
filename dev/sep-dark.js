421af60a5feda036316700f6566ed1e2b9b482c9
Sven Fehler <svenfehler@sv443.net>
2021-12-04 12:40:42 +0100

    fix type imports & type names

--- dev/sep-dark.js
+++ dev/sep-dark.js
@@ -3,26 +3,31 @@ const { readFile, readdir } = require("fs-extra");
 const { statSync } = require("fs");
 const promiseAllSeq = require("promise-all-sequential");
 
+// const settings = require("../settings");
 
-/** @typedef {import("./src/types/jokes").JokesFile} JokesFile */
-/** @typedef {import("./src/types/languages").LangCode} LangCode */
+
+/** @typedef {import("../src/types/jokes").JokesFile} JokesFile */
+/** @typedef {import("../src/types/languages").LangCode} LangCode */
 
 /**
- * @typedef {object} JokeFileObj
- * @prop {JokesFile} file
- * @prop {LangCode} lang
+ * @typedef {object} JokesFileObj Contains the jokes file and some more info about the file
+ * @prop {JokesFile} file Jokes file, represented 1:1 like in the JSON file
+ * @prop {LangCode} lang Language of the jokes
  */
 
 
+/** File paths */
 const paths = {
+    /** Input directory path that contains all jokes files */
     input: resolve("./data/jokes/"),
+    /** Output directory path that will contain the new jokes files with all the dark jokes, that have been extracted from the input files */
     output: resolve("./data/jokes/dark/"),
 };
 
 /**
- * @returns {Promise<JokeFileObj[]>}
+ * @returns {Promise<JokesFileObj[]>}
  */
-async function readJokeFiles()
+async function readJokesFiles()
 {
     const jokeFileNames = await readdir(paths.input);
 
@@ -31,7 +36,7 @@ async function readJokeFiles()
     const filteredPaths = jokeFilePaths.map(n => join(paths.input, n));
 
 
-    /** @type {(() => Promise<JokeFileObj>)[]} */
+    /** @type {(() => Promise<JokesFileObj>)[]} */
     const readProms = [];
 
     filteredPaths.forEach((path) => {
@@ -49,17 +54,17 @@ async function readJokeFiles()
         }));
     });
 
-    /** @type {JokeFileObj[]} */
-    const jokeFiles = await promiseAllSeq(readProms);
+    /** @type {JokesFileObj[]} */
+    const jokesFiles = await promiseAllSeq(readProms);
 
-    return jokeFiles;
+    return jokesFiles;
 }
 
 async function run()
 {
-    const jokeFiles = await readJokeFiles();
+    const jokesFiles = await readJokesFiles();
 
-    console.log(jokeFiles);
+    console.log(jokesFiles);
 }
 
 (() => run())();
