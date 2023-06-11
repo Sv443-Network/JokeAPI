b18df864072bfb52d3c9016515418f77907db709
Sv443 <contact@sv443.net>
2023-06-11 16:16:19 +0200

    ref: 2 space indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,34 @@
 module.exports = {
-    env: {
-        es6: true,
-        node: true,
-    },
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-    ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
-    plugins: [
-        "@typescript-eslint",
-    ],
-    ignorePatterns: [
-        "out/**",
-        "test.*",
-        "src/tools/**",
-    ],
-    rules: {
-        "quotes": [ "error", "double" ],
-        "semi": [ "error", "always" ],
-        "eol-last": [ "error", "always" ],
-        "no-async-promise-executor": "off",
-        // see https://github.com/eslint/eslint/issues/14538#issuecomment-862280037
-        "indent": ["error", 4, { "ignoredNodes": ["VariableDeclaration[declarations.length=0]"] }],
-        "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/no-unused-vars": ["warn", { "ignoreRestSiblings": true, "argsIgnorePattern": "^_" }],
-        "comma-dangle": ["error", "always-multiline"],
-    },
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [
+    "@typescript-eslint",
+  ],
+  ignorePatterns: [
+    "out/**",
+    "test.*",
+    "src/tools/**",
+  ],
+  rules: {
+    "quotes": [ "error", "double" ],
+    "semi": [ "error", "always" ],
+    "eol-last": [ "error", "always" ],
+    "no-async-promise-executor": "off",
+    // see https://github.com/eslint/eslint/issues/14538#issuecomment-862280037
+    "indent": ["error", 2, { "ignoredNodes": ["VariableDeclaration[declarations.length=0]"] }],
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { "ignoreRestSiblings": true, "argsIgnorePattern": "^_" }],
+    "comma-dangle": ["error", "always-multiline"],
+  },
 };
