c1ac0b52c5b4ade65a7ff4247b5a19267d629f92
Sv443 <contact@sv443.net>
2024-03-02 19:56:01 +0100

    feat: commit pls

---
+++ src/format.ts
@@ -0,0 +1,26 @@
+// import type { Submission } from "@prisma/client";
+// import type { Joke } from "./types/index.js";
+
+export {};
+
+// 
+
+// /**
+//  * Formats a joke for submission to the database  
+//  * - Calculates similarity to existing jokes in here
+//  * - Evaluates safe mode property and adds it
+//  * - Adds the joke ID
+//  * - etc.
+//  */
+// export function formatJokeForSubmit(joke: Joke): Submission {
+//   return {
+//     joke: joke.joke,
+//     category: joke.category,
+//     type: joke.type,
+//     lang: joke.lang,
+//     flags: joke.flags ?? [],
+//     nsfw: joke.nsfw,
+//     submitter: joke.submitter,
+//     source: joke.source,
+//   };
+// }
